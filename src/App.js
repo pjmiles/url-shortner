3747022b39cd14af2dad8ddfb781c30c0129e2f9
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-08-22 15:09:52 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import Navbar from './components/navbar/Navbar';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+    <nav>
+      <Link to="/" className="logo">
+        PJmiles
+      </Link>
+
+      <ul>
+        <Link to="/login" className="nav-link">
+          Log in
+        </Link>
+        <Link to="/pricing" className="nav-link">
+          Pricing
+        </Link>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
