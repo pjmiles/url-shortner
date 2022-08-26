1bce77091302c9a4f5e57acc330f7dc98ccf0fbb
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-08-26 17:21:33 +0100

    connected to backend api for signup

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://robot-shortener.herokuapp.com/api/"
+const baseURL = "https://robot-shortener.herokuapp.com/"
 
 const axiosInstance = axios.create({
     baseURL: baseURL
--- src/components/signup/SignUpForm.js
+++ src/components/signup/SignUpForm.js
@@ -1,75 +1,127 @@
 import { useState } from "react";
+import axiosInstance from "../../api/axios";
+import { Link } from "react-router-dom";
 import "./SignUpForm.css";
 
 const SignUpForm = () => {
-  const [signUpUser, setSignUpUser] = useState({
+  const [signUpDetails, setSignUpDetails] = useState({
     name: "",
     email: "",
     password1: "",
     password2: "",
-  })
-  const [sucess, setSuccess] = useState(false) // for successful state upon signing up
-  const [error, setError] = useState("") // to show error messages assiciated with signing up
+  });
+  const [sucess, setSuccess] = useState(false); // for successful state upon signing up
+  const [error, setError] = useState(""); // to show error messages assiciated with signing up
+
   const handleSignUpChange = (e) => {
-    setSignUpUser((prev) => ({
-      ...prev, [e.target.value]: e.target.value.trim()
-    }))
-  }
-  
+    setSignUpDetails((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const postRequest = async () => {
+    try {
+      await axiosInstance.post("dj-rest-auth/registration/", signUpDetails);
+      setSuccess(true);
+    } catch (e) {
+      setSuccess(false);
+      setError("Registration failed, please try again.");
+      console.log(e);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (
+      !signUpDetails.name ||
+      !signUpDetails.email ||
+      !signUpDetails.password1 ||
+      !signUpDetails.password2
+    ) {
+      setError("Please provide details");
+      setSuccess(false);
+    } else if (signUpDetails.password1 !== signUpDetails.password2) {
+      setError("Password does not match");
+      setSuccess(false);
+    } else if (signUpDetails.password1.length <= 7) {
+      setError("Password must be more than 7 characters");
+      setSuccess(false);
+    } else {
+      postRequest();
+    }
+  };
 
   return (
-    <div className="signup-container">
-      <form className="signup-form">
-        <h1 className="signup header-text">SignUp</h1>
-        <div className="signup-control">
-          <label htmlFor="name" />
-          <input
-            type="text"
-            placeholder="Name"
-            name="name"
-            id="name"
-            className="input-field"
-            required
-          />
+    <>
+      {sucess ? (
+        <p>
+          Please Login <Link to="/login">Here</Link>
+        </p>
+      ) : (
+        <div className="signup-container">
+          <form className="signup-form" onSubmit={handleSubmit}>
+            <h1 className="signup header-text">SignUp</h1>
+            {error && <span className="signup-error">{error}</span>}
+            <div className="signup-control">
+              <label htmlFor="name" />
+              <input
+                type="text"
+                placeholder="Name"
+                name="name"
+                id="name"
+                className="input-field"
+                value={signUpDetails.name}
+                onChange={handleSignUpChange}
+                required
+              />
+            </div>
+            <div className="signup-control">
+              <label htmlFor="email" />
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                id="email"
+                className="input-field"
+                value={signUpDetails.email}
+                onChange={handleSignUpChange}
+                required
+              />
+            </div>
+            <div className="signup-control">
+              <label htmlFor="password1" />
+              <input
+                type="password"
+                placeholder="Password"
+                name="password1"
+                id="password1"
+                className="input-field"
+                value={signUpDetails.password1}
+                onChange={handleSignUpChange}
+                required
+              />
+            </div>
+            <div className="signup-control">
+              <label htmlFor="password2" />
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                name="password2"
+                id="password2"
+                className="input-field"
+                value={signUpDetails.password2}
+                onChange={handleSignUpChange}
+                required
+              />
+            </div>
+            <div className="btn-container">
+              <button className="signup-btn">Submit</button>
+            </div>
+          </form>
         </div>
-        <div className="signup-control">
-          <label htmlFor="email" />
-          <input
-            type="email"
-            placeholder="Email"
-            name="email"
-            id="email"
-            className="input-field"
-            required
-          />
-        </div>
-        <div className="signup-control">
-          <label htmlFor="password1" />
-          <input
-            type="password"
-            placeholder="Password"
-            name="password1"
-            id="password1"
-            className="input-field"
-            required
-          />
-        </div>
-        <div className="signup-control">
-          <label htmlFor="password2" />
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            name="password2"
-            id="password2"
-            className="input-field"
-            required
-          />
-        </div>
-        <div className="btn-container">
-          <button className="signup-btn">Submit</button>
-        </div>
-      </form>
-    </div>
+      )}
+    </>
   );
 };
 
